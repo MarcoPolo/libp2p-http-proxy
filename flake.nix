f923fff308401cc60e58dd9d111ac4cae1d7390b
Marco Munizaga <git@marcopolo.io>
2023-06-19 12:22:51 -0700

    First commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in
+      {
+        packages.hello = pkgs.hello;
+        defaultPackage = self.packages.${system}.hello;
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.go ];
+        };
+      });
+}
